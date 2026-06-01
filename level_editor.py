b392fcedf8cc661b8727456daf76ccb535d5505b
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-06-01 13:24:27 +0900

    Update level_editor.py
    TL1_01-04完了

--- level_editor.py
+++ level_editor.py
@@ -14,12 +14,57 @@ bl_info = {
     "category": "Object"
 }
 
-#アドオン有効化時
+#メニュー項目描画
+def draw_menu_manual(self,context):
+    #self : 呼び出し元のクラスインスタンス。C++でいうthisポインタ
+    #context : カーソルを合わせた時のポップアップのカスタマイズなどに使用
+
+    #トップバーの「エディターメニュー」に項目(オペレータ)を追加
+    self.layout.operator("wm.url_open_preset",text ="Manual",icon='HELP')
+
+#トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    #Blenderがクラスを識別する為の固有の文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+    #メニューのラベルとして表示される文字列
+    bl_label = "MyMenu"
+    #著者表示用の文字列
+    bl_description = "拡張メニュー by " + bl_info["author"]
+    
+    #サブメニューの描画
+    def draw(self,context):
+        
+        #トップバーの「エディターメニュー」に項目(オペレータ)を追加
+        self.layout.operator("wm.url_open_preset",text = "Manual",icon ='HELP')
+        
+    #既存のメニューにサブメニューを追加
+    def submenu(self,context):
+        
+        #ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+        
+#Blenderに登録するクラスリスト
+classes = (TOPBAR_MT_my_menu,)
+    
+    
+
+#Add-On有効化時コールバック
 def register():
+    #Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    #メニューから項目を追加
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディダが有効化されました。")
 
 #アドオン無効化時コールバック
 def unregister():
+    #メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    
+    #Blenderにクラスを削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("レベルエディダが無効化されました。")
 
 #テスト実行用コード
