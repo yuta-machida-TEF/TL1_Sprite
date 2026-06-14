542c8b62a92f9ffcbb92fd7ae9ca4616a6b572a5
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-06-14 21:53:19 +0900

    Update level_editor.py
    TL1_01_06完了

--- level_editor.py
+++ level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 #ブレンダーに登録するアドオン情報
 bl_info = {
@@ -42,6 +43,10 @@ class TOPBAR_MT_my_menu(bpy.types.Menu):
         self.layout.operator(MYADDON_OT_create_ico_sphere.bl_idname,
                              text=MYADDON_OT_create_ico_sphere.bl_label)
 
+         #トップバーの「エディターメニュー」に項目(オペレータ)を追加
+        self.layout.operator(MYADDON_OT_export_scene.bl_idname,
+                             text=MYADDON_OT_export_scene.bl_label)
+
     #既存のメニューにサブメニューを追加
     def submenu(self,context):
         
@@ -79,10 +84,48 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
         #オペレータの命令終了を通知
         return  {'FINISHED'}
 
+#オペレータ　シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self,context):
+
+        print("シーン情報をExportします")
+        
+        #シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            #ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
+            #型は Vector, Quaternion, Vector
+            trans, rot, scale = object.matrix_local.decompose()
+            #回転を Quternion から Euler (3軸での回転角) に変換
+            rot = rot.to_euler()
+            #ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            #トランスフォーム情報を表示
+            print("Trans(%f,%f,%f)" % (trans.x,trans.y,trans.z))
+            print("Rot(%f,%f,%f)" % (rot.x , rot.y , rot.z))
+            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
+            #親オブジェクトの名前を表示
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+
+        print("シーン情報をExportしました")
+        self.report({'INFO'},"シーン情報をExportしました")
+
+        #オペレータの命令終了を通知
+        return  {'FINISHED'}
+
 #Blenderに登録するクラスリスト
 classes = (
     MYADDON_OT_staretch_vertex,
     MYADDON_OT_create_ico_sphere,
+    MYADDON_OT_export_scene,
     TOPBAR_MT_my_menu,)
     
     
