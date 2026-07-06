ffbf17158247741f44b64434d2c220f99e73fd01
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-07-06 13:02:54 +0900

    Update level_editor.py
    TL1_01_09修正完了

--- level_editor.py
+++ level_editor.py
@@ -258,11 +258,13 @@ class DrawCollider:
 
         #現在シーンのオブジェクトリストを走査
         for object in bpy.context.scene.objects:
+            
             #追加前の頂点数
             start = len(vertices["pos"])
 
             #Boxの8頂点分回す
             for offset in offsets:
+
                 #オブジェクトの中心座標をコピー
                 pos = copy.copy(object.location)
 
@@ -270,36 +272,38 @@ class DrawCollider:
                 pos[0]+=offset[0]*size[0]
                 pos[1]+=offset[1]*size[1]
                 pos[2]+=offset[2]*size[2]
+
                 #頂点データリストに座標を追加
                 vertices['pos'].append(pos)
-                #前面を構成する辺の頂点インデックス
-                indices.append([start+0,start+1])
-                indices.append([start+2,start+3])
-                indices.append([start+0,start+2])
-                indices.append([start+1,start+3])
-                #奥面を構成する辺の頂点インデックス
-                indices.append([start+4,start+5])
-                indices.append([start+6,start+7])
-                indices.append([start+4,start+6])
-                indices.append([start+5,start+7])
-                #手前と奥を繋ぐ辺の頂点インデックス
-                indices.append([start+0,start+4])
-                indices.append([start+1,start+5])
-                indices.append([start+2,start+6])
-                indices.append([start+3,start+7])
 
-        #ビルドインのシェーダを取得
-        shader = gpu.shader.from_builtin("UNIFORM_COLOR")
+            #前面を構成する辺の頂点インデックス
+            indices.append([start+0,start+1])
+            indices.append([start+2,start+3])
+            indices.append([start+0,start+2])
+            indices.append([start+1,start+3])
+            #奥面を構成する辺の頂点インデックス
+            indices.append([start+4,start+5])
+            indices.append([start+6,start+7])
+            indices.append([start+4,start+6])
+            indices.append([start+5,start+7])
+            #手前と奥を繋ぐ辺の頂点インデックス
+            indices.append([start+0,start+4])
+            indices.append([start+1,start+5])
+            indices.append([start+2,start+6])
+            indices.append([start+3,start+7])
 
-        #バッチを作成(引数 : シェーダ、 トポロジー、 頂点データ、 インデックスデータ)
-        batch = gpu_extras.batch.batch_for_shader(shader, "LINES", vertices,indices = indices)
+            #ビルドインのシェーダを取得
+            shader = gpu.shader.from_builtin("UNIFORM_COLOR")
 
-        #シェーダのパラメータ設定
-        color = [0.5, 1.0, 1.0, 1.0]
-        shader.bind()
-        shader.uniform_float("color", color)
-        #描画
-        batch.draw(shader)
+            #バッチを作成(引数 : シェーダ、 トポロジー、 頂点データ、 インデックスデータ)
+            batch = gpu_extras.batch.batch_for_shader(shader, "LINES", vertices,indices = indices)
+
+            #シェーダのパラメータ設定
+            color = [0.5, 1.0, 1.0, 1.0]
+            shader.bind()
+            shader.uniform_float("color", color)
+            #描画
+            batch.draw(shader)
 
 
 #Blenderに登録するクラスリスト
