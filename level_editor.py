6e9713f709fe43039599ac0d565ef682c5f77d7f
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-07-13 15:16:44 +0900

    Update level_editor.py
    TL1_01_11完了

--- level_editor.py
+++ level_editor.py
@@ -5,6 +5,7 @@ import gpu
 import gpu_extras.batch
 import copy
 import mathutils
+import json
 
 #ブレンダーに登録するアドオン情報
 bl_info = {
@@ -96,7 +97,42 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
     bl_label = "シーン出力"
     bl_description = "シーン情報をExportします"
     #出力するファイルの拡張子
-    filename_ext = ".scene"    
+    filename_ext = ".json"    
+
+    def export_json(self):
+        """JSON形式でファイルに出力"""
+
+        #保存する情報をまとめるdict
+        json_object_root = dict()
+
+        #ノード名
+        json_object_root["name"] = "scene"
+        #オブジェクトリストを作成
+        json_object_root["objects"] = list()
+
+        #シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+
+            #親オブジェクトがあるものはスキップ(代わりに親から呼び出すから)
+            if(object.parent):
+               continue
+
+            #シーン直下のオブジェクトをルートノード(深さ0)とし、再帰関数で走査
+            self.parse_scene_recursive_json(json_object_root["objects"],object,0)
+
+
+        #オブジェクトをJSON文字列にエンコード
+        json_text = json.dumps(json_object_root,ensure_ascii=False, cls=json.JSONEncoder, indent=4)
+        #コンソールに表示してみる
+        print(json_text)
+
+        #ファイルをテキスト形式で書き出し用にオープン
+        #スコープを抜けると自動的にクローズされる
+        with open(self.filepath,"wt",encoding="utf-8") as file:
+
+            #ファイルに文字列を書き込む
+            file.write(json_text)
+
 
     def write_and_print(self,file,str):
         print(str)
@@ -150,7 +186,8 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
 
     def execute(self,context):
 
-        print("シーン情報をExportします")      
+        print("シーン情報をExportします")
+        self.export_json()      
 
         #ファイルに出力
         self.export()
@@ -162,6 +199,7 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
         return  {'FINISHED'}
 
     def parse_scene_recursive(self,file,object,level):
+       
         """シーン解析用再帰関数"""
 
         #深さ分インデントする(タブを挿入)
@@ -202,6 +240,59 @@ class MYADDON_OT_export_scene(bpy.types.Operator,bpy_extras.io_utils.ExportHelpe
         #子ノートへ進む(深さが1上げる)
         for child in object.children:
             self.parse_scene_recursive(file,child,level + 1)
+
+    def parse_scene_recursive_json(self,data_parent,object,level):
+
+        #シーンのオブジェクト1個分のjsonオブジェクト生成
+        json_object = dict()
+        #オブジェクト種類
+        json_object["type"] = object.type
+        #オブジェクト名
+        json_object["name"] = object.name
+
+        #オブジェクトのローカルトランスフォームから
+        #平行移動、回転、スケールを抽出
+        trans, rot ,scale = object.matrix_local.decompose()
+        #回転を Quternion　から　Euler (3軸での回転角)　に変換
+        rot = rot.to_euler()
+        #ラジアンから度数法に変換
+        rot.x = math.degrees(rot.x)
+        rot.y = math.degrees(rot.y)
+        rot.z = math.degrees(rot.z)
+        #トランスフォーム情報をディクショナリに登録
+        transform = dict()
+        transform["translation"] = (trans.x,trans.y,trans.z)
+        transform["rotation"] = (rot.x,rot.y,rot.z)
+        transform["scaling"] = (scale.x,scale.y,scale.z)
+        #まとめて1個分のjsonオブジェクトに登録
+        json_object["transform"] = transform
+
+        #カスタムプロパティ 'file_name'
+        if "file_name" in object:
+            json_object["file_name"] = object["file_name"]
+
+        #カスタムプロパティ 'collider'
+        if "collider" in object:
+            collider = dict()
+            collider["type"] = object["collider"]
+            collider["center"] = object["collider_center"].to_list()
+            collider["size"] = object["collider_size"].to_list()
+            json_object["collider"] = collider
+
+
+        #1個分のjsonオブジェクトを親分オブジェクトに登録
+        data_parent.append(json_object)
+
+        #子ノードリストがあれば
+        if len(object.children) > 0:
+            #子ノードリストを作成
+            json_object["children"] = list()
+
+            #子ノードへ進む(深さ1上がる)
+            for child in object.children:
+                self.parse_scene_recursive_json(json_object["children"],child,level + 1)
+
+
 
 #オペレータ　カスタムプロパティ['file_name']追加
 class MYADDON_OT_add_filename(bpy.types.Operator):
